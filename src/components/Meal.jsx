c296cfbdc10f50b9a59dfd04f004adfc051a2966
Pasha3amok <pasha19850@gmail.com>
2024-03-23 21:40:33 +0200

    add button was added

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,18 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Meal(props) {
-	const {idMeal,strMeal, strMealThumb} = props;
-	return <div className="card">
-	<div className="card-image">
-	  <img src={strMealThumb} alt={strMeal}/>
-	</div>
-	<div className="card-content">
-	<span className="card-title red-text">{strMeal}</span>
-	</div>
-	<div className='card-action'>
-		<Link to={`/meal/${idMeal}`} className='btn'>Watch recipe.</Link>
-	</div>
-  </div>
+    const { idMeal, strMeal, strMealThumb } = props;
+    return (
+        <div className="card">
+            <div className="card-image">
+                <img src={strMealThumb} alt={strMeal} />
+            </div>
+            <div className="card-content">
+                <span className="card-title red-text">{strMeal}</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light red">
+                    <i class="material-icons">add</i>
+                </a>
+            </div>
+            <div className="card-action">
+                <Link to={`/meal/${idMeal}`} className="btn">
+                    Watch recipe.
+                </Link>
+            </div>
+        </div>
+    );
 }
 
-export {Meal};
+export { Meal };
